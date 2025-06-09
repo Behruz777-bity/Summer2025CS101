f1feae5a3101d671e56aa26c208b962258c4624f
Behruz777-bity <b.babajanov@newuu.uz>
2025-06-09 18:52:55 +0500

    https://github.com/Behruz777-bity/Summer2025CS101.git

---
+++ src/week1/lab2/problems 1 to 7.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // p1
+
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<"sum: "<<a+b<<endl;
+    // return 0;
+
+    //p2
+
+    // float x,y;
+    // cin>>x>>y;
+    // float t=(3*(x*x+3))/(y/4+3);
+    // cout<<t<<endl;
+
+
+    //p3
+
+    // int a,b;
+    // cin>>a>>b;
+    // cout<<"is a<b?"<<(a<b)<<endl;
+    // cout<<"is b<a?"<<(b<a)<<endl;
+    // cout<<"is a==b?"<<(b==a)<<endl;
+
+    //p4
+
+    // float v0,v1,t;
+    // cin>>v1>>v0>>t;
+    // double a=(v1-v0)/t;
+    // cout<<a<<endl;
+
+    //p5
+
+    // float num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+    // num--;
+    // num=num*7;
+    // num=14-num;
+    // num=num/12;
+    // num++;
+    // cout << num << endl;
+    // float r=num/3;
+    // cout << r << endl;
+
+
+    //problem 6
+//     float d;
+//     cin >> d;
+//     float a = 4*3.14*d/2*d/2;
+//     cout << a << endl;
+//
+
+// problem 7
+//     int a,b,c,d,sum1, sum2;
+//     cout<<"Enter 4 numbers: ";
+//     cin>>a>>b>>c>>d;
+//
+//     sum1=a+b;
+//     sum2=c+d;
+//
+//     cout<< "Is first sum1 < sum2"<< (sum1<sum2)<<endl;
+//     cout<< "Is first sum1 > sum2" <<(sum1>sum2)<<endl;
+//     cout<< "Are the sums equal?" <<(sum1==sum2)<<endl;
+}
---
+++ src/week2/lab1/problems 1 to 8.cpp
@@ -0,0 +1,129 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main() {
+    // int n;
+    // cout << "Enter a number: ";
+    // cin >> n;
+    // if(n < 0) {
+    //     cout <<n<<" is negative"<< endl;
+    // }
+    // else if (n > 0) {
+    //     cout <<n<<" is positive"<< endl;
+    // }
+    // else {
+    //     cout <<n<<" is zero"<< endl;
+    // }
+
+
+    // int a,b,c;
+    // cout << "Enter  numbers: ";
+    // cin >>a>>b>>c;
+    // if(a>b&&a>c) {
+    //     cout <<a<<" is largest"<< endl;
+    // }
+    // else if (b>a&&b>c) {
+    //     cout <<b<<" is largest"<< endl;
+    // }
+    // else {
+    //     cout <<c<<" is largest"<< endl;
+    // }
+
+
+//  int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+//     if(n% 3 == 0 && n % 5 == 0) {
+//         cout << "FizzBuzz" << endl;
+//     }
+// else if(n % 3 == 0 && n % 5 != 0) {
+//     cout << "Fizz" << endl;
+// }
+//     else if(n % 3 != 0 && n % 5 == 0) {
+//         cout << "Buzz" << endl;
+//     }
+//     else {
+//         cout << "Error" << endl;
+//     }
+
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // float d=(b*b)-(4*a*c);
+    // if (d<0) {
+    //     cout<<"roots are imaginary";
+    // }
+    // else {
+    //    float x1= (-b + sqrt(d)) / (2.0 * a);
+    //     float x2= (-b - sqrt(d)) / (2.0 * a);
+    //     cout<<"roots are "<<x1<<" "<<x2<<endl;
+    // }
+
+
+    // int a,b,c;
+    // cout << "Enter  sides of triangle ";
+    // cin >>a>>b>>c;
+    // if(a+b<=c || a+c<=b || b+c<=a) {
+    //     cout<<"not a triangle"<<endl;
+    // }
+    // else {
+    //     if (a==b || b==c) {
+    //         cout<<"equilateral"<<endl;
+    //     }
+    //     else if (a==b || a==c) {
+    //         cout<<"isosceles"<<endl;
+    //     }
+    //     else {
+    //         cout<<"scalene"<<endl;
+    //     }
+    // }
+
+
+    // char c;
+    // cout << "Enter a character: ";
+    // cin >> c;
+    // if (c >= 'a' && c <= 'z') {
+    //     cout << "lowercase letter" << endl;
+    // }
+    // else if (c >= 'A' && c <= 'Z') {
+    //     cout << "uppercase letter" << endl;
+    // }
+    // else {
+    //     cout << "none" << endl;
+    // }
+
+
+    // int x;
+    // cin>>x;
+    // int a=x/100;
+    // int b=x%10;
+    // if (a==b) {
+    //     cout<<"polindrome";
+    // }
+    // else {
+    //         cout<<"not a polindrome";
+    //     }
+
+
+        //
+        // int number;
+        // cout << "Enter a 4-digit positive integer: ";
+        // cin >> number;
+        //
+        // if (number < 1000 || number > 9999) {
+        //     cout << "Please enter a 4-digit number." << endl;
+        //     return 0;
+        // }
+        // int countEven = 0;
+        // int d1 = number / 1000;
+        // int d2 = (number / 100) % 10;
+        // int d3 = (number / 10) % 10;
+        // int d4 = number % 10;
+        //
+        // if (d1 % 2 == 0) countEven++;
+        // if (d2 % 2 == 0) countEven++;
+        // if (d3 % 2 == 0) countEven++;
+        // if (d4 % 2 == 0) countEven++;
+        //
+        // cout << "Number of even digits: " << countEven << endl;
+}
