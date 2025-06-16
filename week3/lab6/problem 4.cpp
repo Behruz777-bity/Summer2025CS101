2febe28dfe68b8e213598b59033ba49258117469
b.babajanov <b.babajanov@newuu.uz>
2025-06-16 16:54:38 +0500

    lab 6

---
+++ week3/lab6/problem 4.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Behruz Babajanov on 16/06/25.
+//
+// #include <iostream>
+// using namespace std;
+//
+// void fun(double a , double b) {
+//     cout << "area = " << a * b << endl;
+//     cout <<"perimeter = " << a + b << endl;
+//
+// }
+//
+// int main() {
+//     double a,b;
+//     cin >> a>>b;
+//     fun(a,b);
+//     return 0;
+// }
---
+++ week3/lab6/problem 5.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Behruz Babajanov on 16/06/25.
+//
+// #include <iostream>
+//  using namespace std;
+// int factorial(int x=0) {
+//     return x;
+// }
+// int main() {
+//     int num;
+//     cin >> num;
+//     cout <<factorial(x: num)<<endl;
+// }
